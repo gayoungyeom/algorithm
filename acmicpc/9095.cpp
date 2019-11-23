9293b9d14d420c53e7093bbec31fbf729d525aff
Gayoung Yeom <gayoungyeom@gmail.com>
2019-11-24 02:53:07 +0900

    DP(bottom-up)

--- acmicpc/9095.cpp
+++ acmicpc/9095.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int f[11];
 
 int dp(int n){
-	f[1]=1;
-	f[2]=2;
-	f[3]=4;
+	f[1] = 1;
+	f[2] = 2;
+	f[3] = 4;
 	for(int i=4; i<=n; i++)
 		f[i] = f[i-1] + f[i-2] + f[i-3];
 	return f[n]; 
