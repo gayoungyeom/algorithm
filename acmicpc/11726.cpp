7123f078d6c51265a7a23acc9f9ca3f72e85d193
Gayoung Yeom <gayoungyeom@gmail.com>
2019-11-27 16:06:27 +0900

    DP(memoization)

---
+++ acmicpc/11726.cpp
@@ -0,0 +1,22 @@
+/*
+ *f[i]: 2*i 크기의 직사각형을 채우는 방법의 수
+ */
+#include <iostream>
+using namespace std;
+
+int f[1001];
+
+int dp(int n){
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+    if(f[n] != 0) return f[n];
+    f[n] = (dp(n-1) + dp(n-2)) % 10007;
+    return f[n];
+}
+
+int main(){
+	int n;
+	cin>>n;
+	cout<<dp(n);
+    return 0;
+}
