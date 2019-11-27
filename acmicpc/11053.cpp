926cb62c7be7b3f9431e1c398311fabe6ff1db53
Gayoung Yeom <gayoungyeom@gmail.com>
2019-11-27 16:35:16 +0900

    DP(bottom-up)

---
+++ acmicpc/11053.cpp
@@ -0,0 +1,33 @@
+/*
+ * length[i]: Ai일 때 가장 긴 증가하는 부분 수열
+ */
+
+#include <iostream>
+using namespace std;
+
+int A[1001];
+int length[1001];
+
+int dp(int n){
+	int answer = 1;
+	length[1] = 1;
+	for(int i=2; i<=n; i++){
+		int max = 0;
+		for(int j=i-1; j>0; j--){
+			if(A[j] < A[i] && length[j] > max) max = length[j];
+		}
+		length[i] = max + 1;
+		if(length[i] > answer) answer = length[i];
+	}
+	return answer;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1; i<=n; i++) cin>>A[i];
+
+	cout<<dp(n);
+
+	return 0;
+}
