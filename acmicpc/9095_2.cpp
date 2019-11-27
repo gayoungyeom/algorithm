1127d07c9cfa739400b0fab317f6bb939c25b9f6
Gayoung Yeom <gayoungyeom@gmail.com>
2019-11-27 16:19:23 +0900

    DP(memoization)

--- acmicpc/9095_2.cpp
+++ acmicpc/9095_2.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int f[11];
 
 int dp(int n){
-	if(n==1) return 1;
-	if(n==2) return 2;
-	if(n==3) return 4;
-	if(f[n]!=0) return f[n];
+	if(n == 1) return 1;
+	if(n == 2) return 2;
+	if(n == 3) return 4;
+	if(f[n] != 0) return f[n];
 	return f[n] = dp(n-1) + dp(n-2) + dp(n-3);
 }
 
