776437a139813846f32b963bcf270ec7ee126a1f
Gayoung Yeom <gayoungyeom@gmail.com>
2020-01-22 02:57:30 +0900

    LCS 2/ DP(bottom-up)

---
+++ acmicpc/9252.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+	string X;
+	string Y;
+	stack<char> lcs;
+	cin>>X>>Y;
+
+	X = '0' + X;
+	Y = '0' + Y;
+	int m = X.length();
+	int n = Y.length();
+	int length[m][n];
+
+	//base case
+	for(int i=0; i<n; i++)
+		length[0][i] = 0;
+	for(int i=0; i<m; i++)
+		length[i][0] = 0;
+
+	for(int i=1; i<m; i++){
+		for(int j=1; j<n; j++){
+			if(X[i] == Y[j])
+				length[i][j] = length[i-1][j-1] +1;
+			else
+				length[i][j] = max(length[i-1][j], length[i][j-1]);
+		}
+	}
+
+	//find lcs
+	int logm = m;
+	int logn = n;
+	while(logm>1 && logn>1){
+		if(length[logm-1][logn-1] == length[logm-1][logn-2])
+			logn--;
+		else if(length[logm-1][logn-1] == length[logm-2][logn-1])
+			logm--;
+		else{ //일치하는 경우(대각선에서 온 경우)
+			lcs.push(X[logm-1]);
+			logn--;
+			logm--;
+		}
+	}
+
+    cout<<length[m-1][n-1]<<endl;
+    while(!lcs.empty()){
+    	cout<<lcs.top();
+    	lcs.pop();
+    }
+
+    return 0;
+}
+
