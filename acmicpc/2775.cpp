0f080409a466869eae7aae71371a14026b202d12
Gayoung Yeom <gayoungyeom@gmail.com>
2020-02-04 15:49:31 +0900

    부녀회장이 될테야/ Math

--- acmicpc/2775.cpp
+++ acmicpc/2775.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int d[15][15];
+
+int dp(int k, int n){
+	for(int i=1; i<=n; i++)
+		d[0][i] = i;
+
+	for(int i=1; i<=k; i++){
+		for(int j=1; j<=n; j++){
+			d[i][j] = d[i-1][j] + d[i][j-1];
+		}
+	}
+	return d[k][n];
+}
+
 int main(){
-	int h, w, n, T;
+	int T, k, n;
 	cin>>T;
 
 	for(int i=0; i<T; i++){
-		cin>>h>>w>>n;
-		int floor = n % h;
-		int ho = n / h + 1;
-		if(floor == 0){
-			floor = h;
-			ho = n / h;
-		}
-		if(ho < 10)	cout<<floor<<0<<ho<<endl;
-		else cout<<floor<<ho<<endl;
+		cin>>k>>n;
+		cout<<dp(k, n)<<endl;
 	}
-
 	return 0;
 }
 
