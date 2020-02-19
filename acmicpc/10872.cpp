dcf40f8cd24baf2ad475c848a408852c21e377c3
Gayoung Yeom <gayoungyeom@gmail.com>
2020-02-19 19:04:16 +0900

    팩토리얼/ 재귀

---
+++ acmicpc/10872.cpp
@@ -0,0 +1,16 @@
+// 0! = 1
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+	if(n < 2) return 1;
+	return fact(n-1) * n;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	cout<<fact(n);
+	return 0;
+}
+
