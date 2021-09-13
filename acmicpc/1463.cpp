6ee85db1afdba56b791c46d813736209add73181
gayoungyeom <gayoungyeom@gmail.com>
2021-09-14 00:58:50 +0900

    1로 만들기/ DP(bottom-up)

--- acmicpc/1463.cpp
+++ acmicpc/1463.cpp
@@ -1,29 +1,25 @@
-/*
- * count[i]: i를 1로 만드는데 최소 연산 횟수
- */
-
 #include <iostream>
 using namespace std;
 
-int count[1000001];
+int f[3000003];
 
-int minCount(int n){
-	count[2] = count[3] = 1;
-	for(int i=4; i<=n; i++){
-		if(i%3 == 0)
-			count[i] = min(count[i/3], count[i-1]) + 1;
-		else if(i%2 == 0)
-			count[i] = min(count[i/2], count[i-1]) + 1;
-		else
-			count[i] = count[i-1] + 1;
+int dp(int x){
+	for(int i=1; i<1000001; i++){
+		if(!f[i+1])	f[i+1] = f[i] + 1;
+		else f[i+1] = min(f[i+1], f[i] + 1);
+
+		if(!f[i*2])	f[i*2] = f[i] + 1;
+		else f[i*2] = min(f[i*2], f[i] + 1);
+
+		if(!f[i*3]) f[i*3] = f[i] + 1;
+		else f[i*3] = min(f[i*3], f[i] + 1);
 	}
-	return count[n];
+	return f[x];
 }
 
 int main(){
-	int n;
-	cin>>n;
-	cout<<minCount(n);
+	int x;
+	cin>>x;
+	cout<<dp(x);
 	return 0;
 }
-
