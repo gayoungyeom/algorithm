4afe4bd3f7cfc7552013d57fc2d187e676fa469e
gayoungyeom <gayoungyeom@gmail.com>
2021-11-18 23:49:17 +0900

    Binary Search

---
+++ hackerrank/climbingTheLeaderboard.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
+    vector<int> ans;
+
+    sort(ranked.begin(), ranked.end());
+    ranked.erase(unique(ranked.begin(), ranked.end()), ranked.end());
+    sort(ranked.rbegin(), ranked.rend());
+    
+    for(unsigned int i=0; i<player.size(); i++){      
+        int left = 0;
+        int right = ranked.size() - 1;
+        int mid;
+        
+        while(left <= right){
+            int mid = (right + left) / 2;
+            if(player[i] >= ranked[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }      
+        ans.push_back(left + 1);  
+    }
+    
+    return ans;    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string ranked_count_temp;
+    getline(cin, ranked_count_temp);
+
+    int ranked_count = stoi(ltrim(rtrim(ranked_count_temp)));
+
+    string ranked_temp_temp;
+    getline(cin, ranked_temp_temp);
+
+    vector<string> ranked_temp = split(rtrim(ranked_temp_temp));
+
+    vector<int> ranked(ranked_count);
+
+    for (int i = 0; i < ranked_count; i++) {
+        int ranked_item = stoi(ranked_temp[i]);
+
+        ranked[i] = ranked_item;
+    }
+
+    string player_count_temp;
+    getline(cin, player_count_temp);
+
+    int player_count = stoi(ltrim(rtrim(player_count_temp)));
+
+    string player_temp_temp;
+    getline(cin, player_temp_temp);
+
+    vector<string> player_temp = split(rtrim(player_temp_temp));
+
+    vector<int> player(player_count);
+
+    for (int i = 0; i < player_count; i++) {
+        int player_item = stoi(player_temp[i]);
+
+        player[i] = player_item;
+    }
+
+    vector<int> result = climbingLeaderboard(ranked, player);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
