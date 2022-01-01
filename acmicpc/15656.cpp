12fc642ae7ec0e68e17822694612c030bf9bb3f7
gayoungyeom <gayoungyeom@gmail.com>
2022-01-01 10:55:50 +0900

    N과 M(7)/ Backtracking

---
+++ acmicpc/15656.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+vector<int> arr;
+
+void backtrack(int n, int m){
+	if((int)v.size() == m){ //재귀 종료 조건
+		for(int i=0; i<m; i++)
+			cout<<v[i]<<" ";
+		cout<<'\n';
+		return;
+	}
+	for(int i=0; i<n; i++){
+		v.push_back(arr[i]);
+		backtrack(n, m);
+		v.pop_back();
+	}
+}
+
+int main(){
+	int n, m;
+	cin>>n>>m;
+
+	int x;
+	for(int i=0; i<n; i++){
+		cin>>x;
+		arr.push_back(x);
+	}
+
+	sort(arr.begin(), arr.end());
+	backtrack(n, m);
+
+	return 0;
+}
+
