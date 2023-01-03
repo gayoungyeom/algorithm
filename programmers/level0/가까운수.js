5ba54e11dc74deb4a649d5687bdf5399d4bb30f5
gayoungyeom <gayoungyeom@gmail.com>
2023-01-04 00:49:32 +0900

    구현(reduce)

--- programmers/level0/가까운수.js
+++ programmers/level0/가까운수.js
@@ -1,6 +1,13 @@
+//1
 function solution(array, n) {  
     array.sort();
     const diff = array.map(a => Math.abs(a - n));
     const minIdx = diff.indexOf(Math.min(...diff));
     return array[minIdx];
+}
+
+//2
+function solution(array, n) {  
+    array.sort((a, b) => a - b);
+    return array.reduce((acc, cur) => Math.abs(acc - n) > Math.abs(cur - n) ? cur : acc, array[0]);
 }
