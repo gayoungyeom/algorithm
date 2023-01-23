f6719803124458a05d7aba74ec99c10a71ae5fbb
gayoungyeom <gayoungyeom@gmail.com>
2023-01-23 18:42:39 +0900

    문자열

---
+++ programmers/level1/문자열을정수로바꾸기.js
@@ -0,0 +1,27 @@
+function solution(s) {
+    return Number(s);
+}
+
+function solution(s) {
+    return parseInt(s);
+}
+
+function solution(s) {
+    return parseFloat(s);
+}
+
+function solution(s) {
+    return Math.floor(s);
+}
+
+function solution(s) {
+    return +s;
+}
+
+function solution(s) {
+    return s*1;
+}
+
+function solution(s) {
+    return ~~s;
+}
