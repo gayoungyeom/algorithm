8bdd7519437208e14f9e563829f79f2ccb1c7d9f
gayoung <gayoungyeom@gmail.com>
2023-06-02 19:51:54 +0900

    BigInt

--- softeer/level2/바이러스.js
+++ softeer/level2/바이러스.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim();
 const [k, p, n] = input.split(' ').map(Number);
 
-const MOD = 1000000007;
+const MOD = BigInt(1000000007);
 function solution(k, p, n){
+    k = BigInt(k); p = BigInt(p);
     while(n--) k = (k * p) % MOD;
-    return k;
+    return Number(k);
 }
 
 console.log(solution(k, p, n));
