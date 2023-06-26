516406c4ce3e6a88d3fc113dd9f5096c9b75b9dc
gayoung <gayoungyeom@gmail.com>
2023-06-26 21:17:39 +0900

    Math

---
+++ programmers/level2/줄서는방법.js
@@ -0,0 +1,27 @@
+function solution(n, k) {
+    let fact = [...Array(n + 1).fill(1)];
+    for(let i = 1; i <= n; i++){
+        fact[i] = fact[i - 1] * i;
+    }
+    
+    const arr = [...Array(n + 1).keys()].slice(1);
+    const result = [];
+    let cur = n;
+    
+    k--;
+    
+    while(result.length !== n){
+        const turn = fact[cur] / cur;
+        const idx = ~~(k / turn);
+        k %= turn;
+        
+        result.push(arr[idx]);
+        arr.splice(idx, 1);
+        
+        cur--;
+    }
+    
+    return result;
+}
+
+
